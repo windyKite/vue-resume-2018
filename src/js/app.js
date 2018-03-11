6d0d4bc51565d38f05eee0901f31ab4cc6ecdfca
萧泳森 <603540681@qq.com>
2018-03-11 14:47:53 +0800

    添加注册、登录、登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -12,18 +12,51 @@ let app = new Vue({
       phone:'18826132439',
       email:'example@example.com',
     },
+    signUp:{
+      email:'',
+      password:'',
+    },
+    login:{
+      email:'',
+      password:'',
+    },
   },
   methods:{
     onEdit(key,value){
       this.resume[key] = value
     },
     onClickSave(){
-      let currentUser = AV.User.current();
-      if (!currentUser) {
-        this.loginVisible = true
-      }else {
-        
+      let currentUser = AV.User.current()
+      if (currentUser) {
+        // 跳转到首页
+      }
+      else {
+        //currentUser 为空时，可打开用户注册界面…
+        this.signUpVisible = true
       }
     },
+    onSignUp(e){
+      var user = new AV.User()
+      user.setUsername(this.signUp.email)
+      user.setPassword(this.signUp.password)
+      user.setEmail(this.signUp.email)
+      user.signUp().then(function (loginedUser) {
+        console.log(loginedUser)
+      }, function (error) {
+        console.log(error)
+      })
+    },
+    onLogout(){
+      AV.User.logOut()
+      alert('注销成功')
+      window.location.reload
+    },
+    onLogin(){
+      AV.User.logIn(this.login.email, this.login.password).then(function (loginedUser) {
+        console.log(loginedUser);
+      }, function (error) {
+        console.log(error)
+      });
+    }
   }
 })
